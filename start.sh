88c2b494a5d07118598dd209457b5bf07655cc3a
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-07-11 23:14:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/FreakersBots/Auto-Filter-With-Url-Shortener-Bot /Eva
+  git clone https://github.com/yuvraj371/Auto-Filter-With-Url-Shortener-Bot/tree/main /Eva
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
